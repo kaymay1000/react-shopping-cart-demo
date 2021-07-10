d03243bbefa577477f94c3b5452f1bf33726ac7b
Kaylee Alvarado <kaylee@assembleinc.com>
2021-07-09 20:08:45 -0700

    count state variable created and is updating properly with plus and minus buttons

--- src/components/FilterableProductTable.jsx
+++ src/components/FilterableProductTable.jsx
@@ -4,12 +4,13 @@ import ProductTable from './ProductTable';
 
 const FilterableProductTable = () => {
 
-  const [inStockOnly, setInStockOnly] = useState(0);
+  // const [inStockOnly, setInStockOnly] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="filterableProductTableWrapper">
       <ProductTableHeader></ProductTableHeader>
-      <ProductTable inStockOnly={inStockOnly}></ProductTable>
+      <ProductTable></ProductTable>
     </div>
    
   );
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,42 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/product.css';
 
 const Product = (props) => {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = { count: 0 }
-  //   this.incrementCount = this.incrementCount.bind(this)
-  //   this.decrementCount = this.decrementCount.bind(this)
-  // }
-
-  // incrementCount() {
-  //   console.log('current count: ', this.state.count);
-  //   let newCount = this.state.count + 1;
-  //   this.setState({count: newCount});
-  //   console.log('new count: ', this.state.count);
-  // }
-
-  // decrementCount() {
-  //   console.log('current count: ', this.state.count);
-  //   let newCount = this.state.count - 1;
-  //   this.setState({count: newCount});
-  //   console.log('new count: ', this.state.count);
-  // }
-
-  console.log('product props obj: ', props);
-  let stockedString = props.stocked === true ? 'In Stock' : 'Out of Stock'
+  let [count, setCount] = useState(0);
+  let stockedValue = props.stocked === true ? 'In Stock' : 'Out of Stock';
   
   return (
     <div key={props.name} className="productItemWrapper">
       <div className="productInfoWrapper">
         <p className="productInfo">{props.name}</p>
         <p className="productInfo">({props.price})</p>
-        <p className="productInfo">{stockedString}</p>
+        <p className="productInfo">{stockedValue}</p>
       </div>
       
       <div className="counterButtonWrapper">
-        <button className="counterButton">+</button>
-        <button className="counterButton">-</button>
+        <button 
+          onClick={() => {
+            count++
+            setCount(count);
+            console.log(props.name, count);
+            }} 
+          className="counterButton">
+            +
+        </button>
+        <button
+          onClick={() => {
+            count--
+            setCount(count);
+            console.log(props.name, count);
+          }} 
+          className="counterButton">
+            -
+        </button>
+        <div className="totalProducts">Total: {count}</div>
       </div>
     </div>
   );
