3ac87cf1b42387731832767899e355009b50f070
Kaylee Alvarado <kaylee@assembleinc.com>
2021-07-15 23:32:19 -0700

    remove console log

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,7 +2,6 @@ import CartNavItem from './CartNavItem';
 import '../styles/navBar.css';
 
 const NavBar = (props) => {
-  console.log('navbar props: ', props)
   return (
     <div className="navBarWrapper">
       <CartNavItem cart={props.cart}/>
